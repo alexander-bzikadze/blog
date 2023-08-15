637823574d214cdb1f532d2887aad95909848d6f
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-08-15 15:57:28 +0900

    Integrate svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte'
 import relativeLinks from "astro-relative-links";
 
 export default defineConfig({
+    build: {
+        format: 'file',
+    },
+    compressHTML: true,
   output: 'static',
-  integrations: [relativeLinks()]
+  trailingSlash: 'never',
+  integrations: [
+    relativeLinks(),
+    svelte({
+      preprocess: [],
+    }),
+  ],
 });
