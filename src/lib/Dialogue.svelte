e5857e7c67de581c85e86240684722e2b31ae1b8
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-10-03 22:44:22 +0900

    Add landing page * Add first version of the landing page; * Update astro version; * Fix svelte plugin to include typescript preprocessor.

---
+++ src/lib/Dialogue.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+  import Message from './Message.svelte'
+  import Button from "./Button.svelte"
+</script>
+
+<script lang="ts">
+  export let messages: string[]
+  export let speaker: string
+
+  export let navigations: { text: string }[]
+</script>
+
+
+<div class="py-2 flex flex-col justify-between  {$$props.class ?? ''}">
+  <div class="flex flex-row items-end gap-2" >
+    <img class="h-auto w-[10%] object-contain rounded-full" src={speaker} alt="" />
+    <div class="flex flex-col gap-2">
+      {#each messages as message}
+        <Message class="max-w-[25rem] " {message}/>
+      {/each}
+    </div>
+  </div>
+  <div class="flex flex-row justify-evenly">
+    {#each navigations as navi}
+      <Button text={navi.text} action={() => alert('Not Implemented')}/>
+    {/each}
+  </div>
+</div>
