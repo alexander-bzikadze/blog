b19af5043c25e97839d225815a11adf6c4d4cde2
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-10-30 23:49:39 +0900

    Add blur to the landpage background and fix button layout

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     export let text: string
     export let action: () => void
-  </script>
+</script>
   
-  <button
+<button
     class="bg-love  border-white text-white bg-opacity-30 glass-morphism p-1 w-fit rounded {$$props.class ?? ''}"
     on:click={action}>
       {text}
