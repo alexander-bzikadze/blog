a51e96b0178541a323ec3ed6d8c208e569415872
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-11-03 02:42:08 +0900

    Add Open Sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: "Open Sans"
+    },
     extend: {
       colors: {
         freedom: "#3B5E68",
