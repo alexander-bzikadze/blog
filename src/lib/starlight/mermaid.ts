1d11c938fadc0299b2b00f6e1f2cab782a286227
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-11-09 20:20:29 +0900

    Fix Mermaid dynamic theme * Create a mermaid facade; * Create a starlight-mermaid adapter; * Remove mermaid two-places dependency; * Move mermaid adapter to starlight components.

---
+++ src/lib/starlight/mermaid.ts
@@ -0,0 +1,26 @@
+import type { ModeProvider } from '../mermaid/facade';
+import { MermaidFacade } from '../mermaid/facade';
+
+const provider: ModeProvider = {
+  get isDark() {
+    return document.documentElement.dataset.theme === 'dark';
+  },
+};
+const facade = new MermaidFacade(provider);
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    facade.renderAll(document.documentElement).catch(console.error);
+  },
+);
+
+const themeObserver = new MutationObserver((records) => {
+  Promise
+    .all(records.map(() => facade.rerenderAll(document.documentElement)))
+    .catch(console.error);
+});
+themeObserver.observe(document.documentElement, {
+  attributes: true,
+  attributeFilter: ['data-theme'],
+});
