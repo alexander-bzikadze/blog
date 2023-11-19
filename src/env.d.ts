9ce09aa9a41a76d6f5d77b35c4e7f44abb31aad2
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-11-20 00:30:48 +0900

    Fix dependabot and add eslint reporting

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,3 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
+/* eslint-disable @typescript-eslint/triple-slash-reference -- This is astro types declaration files */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
