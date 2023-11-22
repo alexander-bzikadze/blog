e1679182778eabdf0698b59d808f3868166b67df
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-11-23 00:03:24 +0900

    Add astro image processing * Adds sharp to process images before serving them; * Adjust Messages.svelte to be more flexible on the icon. * Fix favicon path; * Fix favicon to be "well-formed"; * Add prettier plugin for svgs.

--- src/lib/messages/Messages.svelte
+++ src/lib/messages/Messages.svelte
@@ -5,14 +5,22 @@
 
 <script lang="ts">
   export let messages: string[]
-  export let speaker: string
 </script>
 
-<div class="flex flex-row items-end gap-2 {$$props.class ?? ''}">
-  <img class="h-auto w-10 rounded-full object-contain" alt="Speaker deliveres welcoming speech" src="{speaker}" />
+<div class="flex flex-row items-end gap-2 {$$props['class'] ?? ''}">
+  <div class="icon h-10 w-10">
+    <slot name="icon" />
+  </div>
   <div class="flex flex-col gap-2">
     {#each messages as message}
       <Message class="max-w-[25rem] " {message} />
     {/each}
   </div>
 </div>
+
+<style>
+  .icon :global(*) {
+    border-radius: 9999px;
+    object-fit: contain;
+  }
+</style>
