e1d34fe587e69783a04ba165faafce264eaea12c
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-11-23 22:27:38 +0900

    Connect starlight with material theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,17 @@
 import type { Config } from 'tailwindcss'
+import starlight from '@astrojs/starlight-tailwind'
 import { materialColors, materialTypeface, materialTypescale } from './theme.config.ts'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: (() => {
-    let theme = {}
+    let theme: Config['theme'] = {}
     theme = materialColors.toTailwind(theme)
     theme = materialTypeface.toTailwind(theme)
     theme = materialTypescale.toTailwind(theme)
+    theme = materialColors.toStarlight(theme)
+
     return theme
   })(),
-  plugins: [],
+  plugins: [starlight()],
 } satisfies Config
