a015717b83e2e6527f3c7f6576a088863f3ecb30
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2023-12-22 16:49:11 +0900

    Add portfolio page * Add portfolio page; * Migrate from material web to flowbite and custom components.

---
+++ src/lib/components/chat/Line.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let sender: 'addresser' | 'addressee'
+
+  $: alignment = {
+    addressee: {
+      justify: 'justify-start',
+      items: 'items-start',
+    },
+    addresser: {
+      justify: 'justify-end',
+      items: 'items-end',
+    },
+  }[sender]
+</script>
+
+<div
+  class="
+  flex flex-row gap-2 {alignment.justify} items-end
+  {$$props['class'] ?? ''}">
+  <slot name="avatar" />
+  <div class="flex flex-col gap-2 {alignment.items}">
+    <slot />
+  </div>
+</div>
---
+++ src/lib/components/chat/Message.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import styles from '../styles'
+
+  export let message: string
+
+  export let style: 'primary' | 'secondary' | 'tertiary'
+</script>
+
+<p
+  class="
+    w-fit
+    {styles.colors[style].base}
+    rounded p-2 font-sans text-base font-medium
+    {$$props['class'] ?? ''}">
+  {message}
+</p>
